b90073d90080af7f70f459a812b2c6960c655afe
Youssef Rashad <egyoussef_2000@hotmail.com>
2024-09-17 00:15:54 +0300

    refactor established ground data to start working on the movie profile page

---
+++ src/UI/MovieProfileMain.jsx
@@ -0,0 +1,8 @@
+import "./MovieProfileMain.scss";
+
+const MovieProfileMain = ({ cast, crew, title }) => {
+  console.log(cast);
+  return <div>MovieProfileMain</div>;
+};
+
+export default MovieProfileMain;
--- src/pages/MovieProfile.jsx
+++ src/pages/MovieProfile.jsx
@@ -1,10 +1,30 @@
 import { useLoaderData } from "react-router-dom";
+import MovieProfileMain from "../UI/MovieProfileMain";
 
 const MovieProfile = () => {
   const data = useLoaderData();
   const movieData = data.movieData;
+  const castData= data.movieData.credits.cast;
+  const crewData=data.movieData.credits.crew;
+  console.log(movieData);
+  return (
+    <>
+      <MovieProfileMain
+        title={movieData.title}
+        backdrop={movieData.backdrop_path}
+        genres= {movieData.genres}
+        id={movieData.id}
+        overview={movieData.overview}
+        imdbID={movieData.imdbID}
+        poster={movieData.poster_path}
+        releaseDate={movieData.releaseDate}
+        runtime={movieData.runtime}
+        cast={castData}
+        crew={crewData}
 
-  return <h1>{movieData.title}</h1>;
+      />
+    </>
+  );
 };
 
 export default MovieProfile;
@@ -12,7 +32,7 @@ export default MovieProfile;
 export const loader = async ({ params }) => {
   const id = params.movieID;
 
-  const movieDataURL = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const movieDataURL = `https://api.themoviedb.org/3/movie/${id}?language=en-US&append_to_response=credits`;
   const reviewsURL = `https://api.themoviedb.org/3/movie/${id}/reviews?language=en-US&page=1`;
   const options = {
     method: "GET",

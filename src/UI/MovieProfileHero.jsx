f9e7c3ecc9e40dd6f3857ac1bb04dc60be5c634c
Youssef Rashad <egyoussef_2000@hotmail.com>
2024-09-22 00:23:33 +0300

    feat: added styles for movieprofilehero component main header

--- src/UI/MovieProfileHero.jsx
+++ src/UI/MovieProfileHero.jsx
@@ -20,10 +20,12 @@ const MovieProfileHero = ({
   const [ratingValue, setRatingValue] = useState(0);
 
   const directors = crew.filter(({ job }) => job === "Director");
-  const writers = crew.filter(({ job }) => job === "Screenplay" || job === "Writer");
+  const writers = crew.filter(
+    ({ job }) => job === "Screenplay" || job === "Writer"
+  );
   const formatedRuntime = runtimeFormatter(runtime);
   const releaseYear = releaseDateFormatter(releaseDate);
-console.log(writers)
+
   const handleRating = (rate) => {
     setRatingValue(rate);
   };
@@ -36,48 +38,55 @@ console.log(writers)
       }}
     >
       <img src={`https://image.tmdb.org/t/p/w342${poster}`} />
-      <h1>{title}</h1>
-      <p>
-        <span>{releaseYear + " "}</span>
-        <span>{formatedRuntime + "  "}</span>
-      </p>
-      <p>
-        {genres.map((item) => (
-          <span key={item.id}>{item.name + "  "}</span>
-        ))}
-      </p>
-      <div className="movieRating-container">
-        <a className="imdbInfo" href="https://www.imdb.com/">
-          <img className="imdbLogo" src="/IMDB_Logo_2016.svg" />
-          <span>{imdbRate}</span>
-        </a>
-        <Rating onClick={handleRating} size={25} />
+
+      <div className="movieHero-infoContainer">
+        <h1>{title}</h1>
+
+        <p>
+          <span>{releaseYear + " "}</span>
+          <span>{formatedRuntime + "  "}</span>
+        </p>
+
+        <p>
+          {genres.map((item) => (
+            <span key={item.id}>{item.name + "  "}</span>
+          ))}
+        </p>
+
+        <div className="movieRating-container">
+          <a className="imdbInfo" href="https://www.imdb.com/">
+            <img className="imdbLogo" src="/IMDB_Logo_2016.svg" />
+            <span>{imdbRate}</span>
+          </a>
+          <Rating onClick={handleRating} size={25} />
+        </div>
+        <p>{overview}</p>
+
+        {/* mapping through each elemnt in the director and writers array
+    and formating it to increase redability 
+  */}
+
+        <p>
+          Directed by:
+          {directors.map((director, index) => (
+            <span key={director.id}>
+              {" " + director.name}{" "}
+              {directors.length !== 1 && index !== director.length - 1
+                ? ", "
+                : ""}
+            </span>
+          ))}
+        </p>
+        <p>
+          Written by:
+          {writers.map((writer, index) => (
+            <span key={writer.id}>
+              {" " + writer.name}{" "}
+              {writers.length !== 1 && index !== writers.length - 1 ? ", " : ""}
+            </span>
+          ))}
+        </p>
       </div>
-      <p>{overview}</p>
-
-      {/* mapping through each elemnt in the director and writers array
-          and formating it to increase redability 
-        */}
-
-      <p>
-        Directed by:
-        {directors.map((director, index) => (
-          <span key={director.id}>
-            {director.name}{" "}
-            {directors.length !== 1 && index !== director.length - 1
-              ? ", "
-              : ""}
-          </span>
-        ))}
-      </p>
-      <p>
-        Written by:
-        {writers.map((writer, index) => (
-          <span key={writer.id}>
-            {writer.name} {writers.length !== 1 &&index !== writers.length - 1 ? ", " : ""}
-          </span>
-        ))}
-      </p>
     </div>
   );
 };
